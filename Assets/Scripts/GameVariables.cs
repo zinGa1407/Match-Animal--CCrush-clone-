9465c781b87df70e20265f6d3f4aa2f97cb09c5e
Alexander <alexanderelzinga94@gmail.com>
2019-08-18 00:12:54 +0200

    Prefab and sprite setup
    Prefabs for selected animals
    Backgrounds added to asset folder
    Sprite data set

---
+++ Assets/Scripts/GameVariables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class GameVariables
+{
+    // Grid Size
+    public static int Rows = 12;
+    public static int Columns = 8;
+
+    //Animation Durations
+    public static float AnimationDuration = 0.2f;
+    public static float MoveAnimationDuration = 0.05f;
+    public static float ExplosionAnimationDuration = 0.3f;
+
+    public static float WaitBeforePotentialMatchesCheck = 2f;
+    public static float OpacityAnimationDelay = 0.05f;
+    
+    //Minimum amount for matches
+    public static int MinimumMatches = 3;
+    public static int MinimumMatchesForBonus = 4;
+
+    //Scores
+    public static int Match3Score = 100;
+    //public static int Match4Score = 300;
+    //public static int MatchPlusScore = 500;
+    public static int SubsequelMatchScore = 1000;
+
+}
---
+++ Assets/Scripts/Tiles/Bonus Tiles/BonusTiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[Flags]
+public enum BonusType
+{
+    None,
+    DestroyWholeRowColumn,
+    Explode
+}
+
+public enum GameState
+{
+    None,
+    SelectionStarted,
+    Animating
+}
+
+public static class BonusTiles
+{
+    public static bool ContainsDestroyWholeRowColumn(BonusType bt){
+        return (bt & BonusType.DestroyWholeRowColumn) == BonusType.DestroyWholeRowColumn;
+    }
+
+}
